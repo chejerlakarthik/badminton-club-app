1cc933d6abc78c0264454b3585974482d7021d5a
Karthik Chejerla <chejerlakarthik@gmail.com>
2025-07-13 18:23:18 +1000

    Clean up

--- infrastructure/bin/badminton-club.ts
+++ infrastructure/bin/badminton-club.ts
@@ -4,9 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { BadmintonClubStack } from '../lib/badminton-club-stack';
 
 const app = new cdk.App();
+
+// Constants for AWS account ID and region
+const AWS_ACCOUNT_ID = '762233758459';
+const REGION_AP_SOUTHEAST_2 = 'ap-southeast-2';
+
 new BadmintonClubStack(app, 'BadmintonClubStack', {
     env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT || '762233758459',
-        region: process.env.CDK_DEFAULT_REGION || 'ap-southeast-2',
+        account: process.env.CDK_DEFAULT_ACCOUNT || AWS_ACCOUNT_ID,
+        region: process.env.CDK_DEFAULT_REGION || REGION_AP_SOUTHEAST_2,
     },
 });
